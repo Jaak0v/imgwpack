2bc0dddc4b277c7cd0c60f92338a9fc3ecc58ba8
Jaak0v <jaakovlol@gmail.com>
2022-01-29 20:02:04 +0100

    kody

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Kody stacji meteorologicznych
+#'
+#' @source Instytut Meteorologii i Gospodarki Wodnej.
+#' \url{https://danepubliczne.imgw.pl/}
+#' @format Ramka danych zawiera:
+#' \describe{
+#'  \item{Kod}{Unikatowy kod stacji}
+#'  \item{Stacja}{Nazwa stacji, do które przypisany jest dany kod}
+#' }
+#' @examples
+#' \dontrun{
+#'  kody
+#' }
+"kody"
