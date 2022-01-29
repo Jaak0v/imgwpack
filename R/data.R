372e01d27831b59e95b4b28347999e07811fa7df
Jaak0v <jaakovlol@gmail.com>
2022-01-29 02:41:49 +0100

    Kody stacji

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Kody stacji wykorzystywane do pobierania danych
+#'
+#'@format Ramka danych zawiera:
+#' \describe{
+#'  \item{KOD}{Unikatowy kod stacji}
+#'  \item{Nazwa stacji}{Nazwa danej stacji}
+#' }
+#'
+#' @usage
+#'
+#' @examples
+#' \dontrun{
+#'  Kody
+#'
+#' }
+"Kody"
